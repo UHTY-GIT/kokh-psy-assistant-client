14e337a094ffd0fba89b9bd146ef577e332645e5
Denys Balazh <73472742+denysitis@users.noreply.github.com>
2024-08-22 13:30:10 +0300

    fix bugs in form send page

--- src/components/form/SendFormPage.vue
+++ src/components/form/SendFormPage.vue
@@ -813,7 +813,7 @@ export default {
 
     onMounted(async () => {
       const queryParams = new URLSearchParams(window.location.search);
-      clientId.value = queryParams.get('client');
+      clientId.value = Number(queryParams.get('client'));
       templateId.value = queryParams.get('template');
       await fetchFormData();
     });
@@ -844,11 +844,11 @@ export default {
         // Перевірка типу форми та реєстрація відповідних даних
         if (formType.value === 'consent_couple' || formType.value === 'consent_individual') {
           const response = await apiService.registerClientInformationConsents(clientId.value);
-          answerableId.value = response.data;
+          answerableId.value = response.data.data;
           answerableType.value = 'InformationConsent';
         } else if (formType.value === 'primary_poll_couple' || formType.value === 'primary_poll_individual') {
           const response = await apiService.registerClientPrimaryPoll(clientId.value);
-          answerableId.value = response.data;
+          answerableId.value = response.data.data;
           answerableType.value = 'PrimaryPoll';
         }
 
